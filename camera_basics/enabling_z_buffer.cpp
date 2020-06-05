a875287bd813e99649be1b4e0283d0950e999cf7
markfirmware <abhijeetagnihotri123>
2020-06-05 15:12:00 +0530

    more basic camera functions

---
+++ camera_basics/enabling_z_buffer.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <fstream>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+using namespace std;
+using namespace glm;
+string load_shader(string&);
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+int main(int argc,char *argv[])
+{   
+    string shader_file1 = argv[1];
+    string shader_file2 = argv[2];
+    glewExperimental = true;
+    char infolog[512];
+    if(!glfwInit())
+    {
+        glfwTerminate();
+        return -1;
+    }
+    string vertex_shader = load_shader(shader_file1);
+    string fragment_shader = load_shader(shader_file2);
+    vertex_shader = vertex_shader.substr(0,vertex_shader.length()-2);
+    fragment_shader = fragment_shader.substr(0,fragment_shader.length()-2);
+    const char *vertexshadersource = vertex_shader.c_str();
+    const char *fragmentshadersource = fragment_shader.c_str();
+    glfwWindowHint(GLFW_SAMPLES,4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_COMPAT_PROFILE,GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window;
+    window = glfwCreateWindow(SCR_WIDTH,SCR_HEIGHT,"THREE_COLORED_TRIANGLE",NULL,NULL);
+    if(window == NULL)
+    {
+        glfwTerminate();
+        cout<<"Cannot create window"<<endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = true;
+    if(glewInit()!=GLEW_OK)
+    {
+        return -1;
+    }
+    glfwSetInputMode(window,GLFW_STICKY_KEYS,GL_TRUE);
+    GLuint VS = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(VS,1,&vertexshadersource,NULL);
+    glCompileShader(VS);
+    GLint success;
+    glGetShaderiv(VS,GL_COMPILE_STATUS,&success);
+    if(!success)
+    {   
+        glGetShaderInfoLog(VS,512,NULL,infolog);
+        
+        cout<<"Vertex shader compilation failed:"<<infolog<<endl;
+        return -1;
+    }
+    GLuint FS = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(FS,1,&fragmentshadersource,NULL);
+    glCompileShader(FS);
+    glGetShaderiv(FS,GL_COMPILE_STATUS,&success);
+    if(!success)
+    {   
+        glGetShaderInfoLog(FS,512,NULL,infolog);
+        cout<<"Fragment Shader compilation failed:"<<infolog<<endl;
+        return -1;
+    }
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program,VS);
+    glAttachShader(shader_program,FS);
+    glLinkProgram(shader_program);
+    glGetProgramiv(shader_program,GL_LINK_STATUS,&success);
+    if(!success)
+    {
+        cout<<"Program linking failed\n";
+        return -1;
+    }
+    // float vertices[] = {
+    //     0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+    //     0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+    //     -0.5f, -0.5f, 0.0f,0.0f, 0.0f, // bottom left
+    //     -0.5f,  0.5f, 0.0f,0.0f, 1.0f
+    // };
+    // unsigned int indices[] = {
+    //     0,1,3,
+    //     1,2,3
+    // };
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    GLuint VAO,VBO;
+    glGenVertexArrays(1,&VAO);
+    glGenBuffers(1,&VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    // glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    // glEnableVertexAttribArray(2);
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    string s = "bricks.jpg";
+    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+    unsigned char *data = stbi_load(s.c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    glEnable(GL_DEPTH_TEST);
+    mat4 view = mat4(1.0f);
+    float radius = 10.0f;
+    float camX,camZ;
+    do
+    {
+        glClearColor(0.2f,0.3f,0.3f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glBindTexture(GL_TEXTURE_2D,texture);
+        glUseProgram(shader_program);
+        glBindVertexArray(VAO);
+        camX = sin(glfwGetTime()) * radius;
+        camZ = cos(glfwGetTime()) * radius;
+        mat4 model = mat4(1.0f);
+        mat4 view = mat4(1.0f);
+        mat4 projection = mat4(1.0f);
+        GLuint modelloc = glGetUniformLocation(shader_program,"model");
+        GLuint viewLoc = glGetUniformLocation(shader_program,"view");
+        GLuint projLoc = glGetUniformLocation(shader_program,"projection");
+        model = glm::rotate(model,(float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 1.0f));
+        view  = glm::lookAt(vec3(camX,0.0f,camZ),vec3(0.0f,0.0f,0.0f),vec3(0.0f,1.0f,0.0f));
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glUniformMatrix4fv(modelloc,1,GL_FALSE,value_ptr(model));
+        glUniformMatrix4fv(viewLoc,1,GL_FALSE,value_ptr(view));
+        glUniformMatrix4fv(projLoc,1,GL_FALSE,value_ptr(projection));
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES,0,36);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while(glfwGetKey(window,GLFW_KEY_ESCAPE)!=GLFW_PRESS && 
+    glfwWindowShouldClose(window)==0);
+    glfwTerminate();
+    return 0;
+}
+string load_shader(string &filename)
+{
+    fstream file;
+    string output,line;
+    file.open(filename);
+    if(!file)
+    {
+        return "NULL";
+    }
+    else
+    {
+        while(file)
+        {
+            getline(file,line);
+            output.append(line+"\n");
+        }
+    }
+    file.close();
+    return output;
+}
