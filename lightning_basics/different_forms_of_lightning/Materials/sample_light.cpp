b6107a021e6572d53931f87898f2f269ec2a3f39
markfirmware <abhijeetagnihotri123>
2020-06-09 14:58:19 +0530

    file change

--- lightning_basics/different_forms_of_lightning/Materials/sample_light.cpp
+++ lightning_basics/different_forms_of_lightning/Materials/sample_light.cpp
@@ -196,6 +196,9 @@ int main(int argc,char *argv[])
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
     glGenVertexArrays(1,&lightCube);
     glBindVertexArray(lightCube);
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
