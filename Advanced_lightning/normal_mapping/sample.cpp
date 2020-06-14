37a238beb3ae6d3a6360a20dff2ac44d29824cba
abhijeetagnihotri123 <abhijeetagnihotri5@gmail.com>
2020-06-14 20:56:39 +0530

    plane mapping

---
+++ Advanced_lightning/normal_mapping/sample.cpp
@@ -0,0 +1,354 @@
+#include <iostream>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <fstream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+using namespace std;
+using namespace glm;
+string load_shader(string&);
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+bool gammaEnabled = false;
+bool gammaKeyPressed = false;
+vec3 cameraPos(0.0f,0.0f,3.0f);
+vec3 cameraFront(0.0f,0.0f,-1.0f);
+vec3 cameraUp(0.0f,1.0f,0.0f);
+vec3 lightPos(0.5f,1.0f,0.3f);
+bool firstMouse = true;
+float Yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float Pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float Fov   =  45.0f;
+void framebuffer_size_callback(GLFWwindow*,int,int);
+void mouse_callback(GLFWwindow*,double,double);
+void scroll_callback(GLFWwindow*,double,double);
+void processWindow(GLFWwindow*);
+GLuint loadTexture(char const *);//, bool);
+int main(int argc,char *argv[])
+{   
+    string shader_file1 = argv[1];
+    string shader_file2 = argv[2];
+    glewExperimental = true;
+    char infolog[512];
+    if(!glfwInit())
+    {
+        glfwTerminate();
+        return -1;
+    }
+    string vertex_shader = load_shader(shader_file1);
+    string fragment_shader = load_shader(shader_file2);
+    vertex_shader = vertex_shader.substr(0,vertex_shader.length()-2);
+    fragment_shader = fragment_shader.substr(0,fragment_shader.length()-2);
+    const char *vertexshadersource = vertex_shader.c_str();
+    const char *fragmentshadersource = fragment_shader.c_str();
+    glfwWindowHint(GLFW_SAMPLES,4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_COMPAT_PROFILE,GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window;
+    window = glfwCreateWindow(SCR_WIDTH,SCR_HEIGHT,"THREE_COLORED_TRIANGLE",NULL,NULL);
+    if(window == NULL)
+    {
+        glfwTerminate();
+        cout<<"Cannot create window"<<endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+    glfwSetCursorPosCallback(window,mouse_callback);
+    glfwSetScrollCallback(window,scroll_callback);
+    glfwSetInputMode(window,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
+    glewExperimental = true;
+    if(glewInit()!=GLEW_OK)
+    {
+        return -1;
+    }
+    glfwSetInputMode(window,GLFW_STICKY_KEYS,GL_TRUE);
+    GLuint VS = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(VS,1,&vertexshadersource,NULL);
+    glCompileShader(VS);
+    GLint success;
+    glGetShaderiv(VS,GL_COMPILE_STATUS,&success);
+    if(!success)
+    {   
+        glGetShaderInfoLog(VS,512,NULL,infolog);
+        
+        cout<<"Vertex shader compilation failed:"<<infolog<<endl;
+        return -1;
+    }
+    GLuint FS = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(FS,1,&fragmentshadersource,NULL);
+    glCompileShader(FS);
+    glGetShaderiv(FS,GL_COMPILE_STATUS,&success);
+    if(!success)
+    {   
+        glGetShaderInfoLog(FS,512,NULL,infolog);
+        cout<<"Fragment Shader compilation failed:"<<infolog<<endl;
+        return -1;
+    }
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program,VS);
+    glAttachShader(shader_program,FS);
+    glLinkProgram(shader_program);
+    glGetProgramiv(shader_program,GL_LINK_STATUS,&success);
+    if(!success)
+    {
+        cout<<"Program linking failed\n";
+        return -1;
+    }
+    glEnable(GL_DEPTH_TEST);
+    GLuint quadVAO,quadVBO;
+        glm::vec3 pos1(-1.0f,  1.0f, 0.0f);
+        glm::vec3 pos2(-1.0f, -1.0f, 0.0f);
+        glm::vec3 pos3( 1.0f, -1.0f, 0.0f);
+        glm::vec3 pos4( 1.0f,  1.0f, 0.0f);
+        // texture coordinates
+        glm::vec2 uv1(0.0f, 1.0f);
+        glm::vec2 uv2(0.0f, 0.0f);
+        glm::vec2 uv3(1.0f, 0.0f);  
+        glm::vec2 uv4(1.0f, 1.0f);
+        // normal vector
+        glm::vec3 nm(0.0f, 0.0f, 1.0f);
+
+        // calculate tangent/bitangent vectors of both triangles
+        glm::vec3 tangent1, bitangent1;
+        glm::vec3 tangent2, bitangent2;
+        // triangle 1
+        // ----------
+        glm::vec3 edge1 = pos2 - pos1;
+        glm::vec3 edge2 = pos3 - pos1;
+        glm::vec2 deltaUV1 = uv2 - uv1;
+        glm::vec2 deltaUV2 = uv3 - uv1;
+
+        float f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+        tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+        tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+        tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+
+        bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+        bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+        bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+
+        // triangle 2
+        // ----------
+        edge1 = pos3 - pos1;
+        edge2 = pos4 - pos1;
+        deltaUV1 = uv3 - uv1;
+        deltaUV2 = uv4 - uv1;
+
+        f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+        tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+        tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+        tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+
+
+        bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+        bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+        bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+
+
+        float quadVertices[] = {
+            // positions            // normal         // texcoords  // tangent                          // bitangent
+            pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+            pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+            pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+
+            pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+            pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+            pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
+        };
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(8 * sizeof(float)));
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(11 * sizeof(float)));
+    string s = "brickwall_normal.jpg";
+    GLuint normalMap = loadTexture(s.c_str());
+    do
+    {
+        glClearColor(0.1f,0.1f,0.1f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        processWindow(window);
+        // glBindTexture(GL_TEXTURE_2D,texture);
+        glUseProgram(shader_program);
+        glBindVertexArray(quadVAO);
+        mat4 model = mat4(1.0f);
+        mat4 view = mat4(1.0f);
+        mat4 projection = mat4(1.0f);
+        GLuint modelloc = glGetUniformLocation(shader_program,"model");
+        GLuint viewLoc = glGetUniformLocation(shader_program,"view");
+        GLuint projLoc = glGetUniformLocation(shader_program,"projection");
+        GLuint lightloc = glGetUniformLocation(shader_program,"lightPos");
+        GLuint vPos = glGetUniformLocation(shader_program,"viewPos");
+        model = glm::rotate(model,(float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 1.0f));
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glUniformMatrix4fv(modelloc,1,GL_FALSE,value_ptr(model));
+        glUniformMatrix4fv(viewLoc,1,GL_FALSE,value_ptr(view));
+        glUniformMatrix4fv(projLoc,1,GL_FALSE,value_ptr(projection));
+        glUniform3fv(lightloc,1,value_ptr(lightPos));
+        glUniform3fv(vPos,1,value_ptr(cameraPos));
+        glBindVertexArray(quadVAO);
+        glDrawArrays(GL_TRIANGLES,0,6);
+        glBindVertexArray(0);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while(glfwGetKey(window,GLFW_KEY_ESCAPE)!=GLFW_PRESS && 
+    glfwWindowShouldClose(window)==0);
+    glfwTerminate();
+    return 0;
+}
+string load_shader(string &filename)
+{
+    fstream file;
+    string output,line;
+    file.open(filename);
+    if(!file)
+    {
+        return "NULL";
+    }
+    else
+    {
+        while(file)
+        {
+            getline(file,line);
+            output.append(line+"\n");
+        }
+    }
+    file.close();
+    return output;
+}
+void processWindow(GLFWwindow *window)
+{
+    float cameraSpeed = 2.5 * 0.1;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && !gammaKeyPressed)
+    {
+        gammaEnabled = !gammaEnabled;
+        gammaKeyPressed = true;
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE)
+    {
+        gammaKeyPressed = false;
+    }
+}
+void mouse_callback(GLFWwindow *window,double xpos,double ypos)
+{
+    if(firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    float xOffset = xpos - lastX;
+    float yOffset = ypos - lastY;
+    lastX = xpos;
+    lastY = ypos;
+    float sensitivity = 0.1;
+    xOffset *= sensitivity;
+    yOffset *= sensitivity;
+
+    Yaw += xOffset;
+    Pitch += yOffset;
+
+    if(Pitch > 89.9)
+    {
+        Pitch = 89.9;
+    }
+    if(Pitch < -89.9)
+    {
+        Pitch = -89.9;
+    }
+    glm::vec3 front;
+    front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    front.y = sin(glm::radians(Pitch));
+    front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    cameraFront = glm::normalize(front);
+}
+void scroll_callback(GLFWwindow *window,double xoffset,double yoffset)
+{
+    Fov -= (float)yoffset;
+    if(Fov < 1.0)
+    {
+        Fov = 1.0f;
+    }
+    if(Fov > 45.0)
+    {
+        Fov = 45.0f;
+    }
+}
+void framebuffer_size_callback(GLFWwindow *window,int width,int height)
+{
+    glViewport(0,0,width,height);
+}
+GLuint loadTexture(char const * path)//, bool gammaCorrection)
+{
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum internalFormat;
+        GLenum dataFormat;
+        if (nrComponents == 1)
+        {
+            internalFormat = dataFormat = GL_RED;
+        }
+        else if (nrComponents == 3)
+        {
+            internalFormat = GL_RGB;
+            dataFormat = GL_RGB;
+        }
+        else if (nrComponents == 4)
+        {
+            internalFormat =  GL_RGBA;
+            dataFormat = GL_RGBA;
+        }
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, dataFormat, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+    return textureID;
+}
