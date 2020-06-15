7fefc9b911302b2c6d4047cac3931758fc9615f0
abhijeetagnihotri123 <abhijeetagnihotri123>
2020-06-15 17:26:13 +0530

    changed file

--- lightning_basics/different_forms_of_lightning/pointlight/compile.cpp
+++ lightning_basics/different_forms_of_lightning/pointlight/compile.cpp
@@ -4,7 +4,7 @@ int main(int argc,char *argv[])
 {   
     string str1 = argv[1];
     string str3 = "g++ "+str1+" -o a ";
-    string str2 = "-lglut -lGL -lGLU -lglfw -lrt -lm -ldl -lXrandr -lXinerama -lXcursor -lXext -lXrender -lXfixes -lX11 -lpthread -lxcb -lXau -lXdmcp -lGLEW -lGLU -lGL -lm -ldl -ldrm -lXdamage -lX11-xcb -lxcb-glx -lxcb-dri2 -lxcb-dri3 -lxcb-present -lxcb-sync -lxshmfence -lXxf86vm -lXfixes -lXext -lX11 -lpthread -lxcb -lXau -lXdmcp";
+    string str2 = "-lglut -lGL -lGLU -lglfw -lrt -lm -ldl -lXrandr -lXinerama -lXcursor -lXext -lXrender -lXfixes -lX11 -lpthread -lxcb -lXau -lXdmcp -lGLEW -lGLU -lGL -lm -ldl -lXxf86vm -lXfixes -lXext -lX11 -lpthread -lxcb -lXau -lXdmcp";
     string str4 = str3+str2;
     const char *command = str4.c_str();
     system(command);
