7e82f0c67b9744caa6b237ee942a6b0c39c33edc
abhijeetagnihotri123 <abhijeetagnihotri123>
2020-06-16 13:52:35 +0530

    basic model

---
+++ modelLoading/backup.cpp
@@ -0,0 +1,238 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <fstream>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+using namespace std;
+using namespace glm;
+string load_shader(string&);
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+vec3 cameraPos(0.0f,0.0f,3.0f);
+vec3 cameraFront(0.0f,0.0f,-1.0f);
+vec3 cameraUp(0.0f,1.0f,0.0f);
+vec3 lightPos(1.2f,1.0f,2.0f);
+vec3 SL(1.0f);
+vec3 objectColor(1.0f,0.5f,0.31f);
+bool firstMouse = true;
+float Yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float Pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float Fov   =  45.0f;
+
+void framebuffer_size_callback(GLFWwindow*,int,int);
+void mouse_callback(GLFWwindow*,double,double);
+void scroll_callback(GLFWwindow*,double,double);
+
+int main(int argc,char *argv[])
+{   
+    string shader_file1 = argv[1];
+    string shader_file2 = argv[2];
+    string shader_file3 = argv[3];
+    string shader_file4 = argv[4];
+    glewExperimental = true;
+    char infolog[512];
+    if(!glfwInit())
+    {
+        glfwTerminate();
+        return -1;
+    }
+    string vertex_shader_cube = load_shader(shader_file1);
+    string fragment_shader_cube = load_shader(shader_file2);
+    vertex_shader_cube = vertex_shader_cube.substr(0,vertex_shader_cube.length()-2);
+    fragment_shader_cube = fragment_shader_cube.substr(0,fragment_shader_cube.length()-2);
+    const char *vertexshadersource = vertex_shader_cube.c_str();
+    const char *fragmentshadersource = fragment_shader_cube.c_str();
+    glfwWindowHint(GLFW_SAMPLES,4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_COMPAT_PROFILE,GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window;
+    window = glfwCreateWindow(SCR_WIDTH,SCR_HEIGHT,"THREE_COLORED_TRIANGLE",NULL,NULL);
+    if(window == NULL)
+    {
+        glfwTerminate();
+        cout<<"Cannot create window"<<endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+    glfwSetCursorPosCallback(window,mouse_callback);
+    glfwSetScrollCallback(window,scroll_callback);
+    glfwSetInputMode(window,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
+    glewExperimental = true;
+    if(glewInit()!=GLEW_OK)
+    {
+        return -1;
+    }
+    glfwSetInputMode(window,GLFW_STICKY_KEYS,GL_TRUE);
+    GLuint VS = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(VS,1,&vertexshadersource,NULL);
+    glCompileShader(VS);
+    GLint success;
+    glGetShaderiv(VS,GL_COMPILE_STATUS,&success);
+    if(!success)
+    {   
+        glGetShaderInfoLog(VS,512,NULL,infolog);
+        
+        cout<<"Vertex shader compilation failed:"<<infolog<<endl;
+        return -1;
+    }
+    GLuint FS = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(FS,1,&fragmentshadersource,NULL);
+    glCompileShader(FS);
+    glGetShaderiv(FS,GL_COMPILE_STATUS,&success);
+    if(!success)
+    {   
+        glGetShaderInfoLog(FS,512,NULL,infolog);
+        cout<<"Fragment Shader compilation failed:"<<infolog<<endl;
+        return -1;
+    }
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program,VS);
+    glAttachShader(shader_program,FS);
+    glLinkProgram(shader_program);
+    glGetProgramiv(shader_program,GL_LINK_STATUS,&success);
+    if(!success)
+    {
+        cout<<"Program linking failed\n";
+        return -1;
+    }
+    
+    float vertices[] = {
+        // positions          // normals           // texture coords
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
+    };
+    do
+    {
+        glClearColor(0.1f,0.1f,0.1f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glBindTexture(GL_TEXTURE_2D,texture);
+        glUseProgram(shader_program);
+        glDrawArrays(GL_TRIANGLES,0,36);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while(glfwGetKey(window,GLFW_KEY_ESCAPE)!=GLFW_PRESS && 
+    glfwWindowShouldClose(window)==0);
+    glfwTerminate();
+    return 0;
+}
+string load_shader(string &filename)
+{
+    fstream file;
+    string output,line;
+    file.open(filename);
+    if(!file)
+    {
+        return "NULL";
+    }
+    else
+    {
+        while(file)
+        {
+            getline(file,line);
+            output.append(line+"\n");
+        }
+    }
+    file.close();
+    return output;
+}
+void mouse_callback(GLFWwindow *window,double xpos,double ypos)
+{
+    if(firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    float xOffset = xpos - lastX;
+    float yOffset = ypos - lastY;
+    lastX = xpos;
+    lastY = ypos;
+    float sensitivity = 0.1;
+    xOffset *= sensitivity;
+    yOffset *= sensitivity;
+
+    Yaw += xOffset;
+    Pitch += yOffset;
+
+    if(Pitch > 89.9)
+    {
+        Pitch = 89.9;
+    }
+    if(Pitch < -89.9)
+    {
+        Pitch = -89.9;
+    }
+    glm::vec3 front;
+    front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    front.y = sin(glm::radians(Pitch));
+    front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    cameraFront = glm::normalize(front);
+}
+void scroll_callback(GLFWwindow *window,double xoffset,double yoffset)
+{
+    Fov -= (float)yoffset;
+    if(Fov < 1.0)
+    {
+        Fov = 1.0f;
+    }
+    if(Fov > 45.0)
+    {
+        Fov = 45.0f;
+    }
+}
+void framebuffer_size_callback(GLFWwindow *window,int width,int height)
+{
+    glViewport(0,0,width,height);
+}
